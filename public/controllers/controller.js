de222f97c12988bb317a831dfd4d2505a64e70e3
Steve <steven.marks1@my.jcu.edu.au>
2015-11-21 23:30:02 +1000

    finished adding contacts and deleting contacts functionality. now doing these functions with the database

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -5,11 +5,32 @@ var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http){
     console.log("Hello from Controller!");
 
-    $http.get('/contactlist').success(function(response) {
-        console.log("I got the data I requested");
-        $scope.contactlist = response;
-    });
+    var refresh = function() {
+        $http.get('/contactlist').success(function (response) {
+            console.log("I got the data I requested");
+            $scope.contactlist = response;
+            $scope.contact = "";
+        });
+    };
 
+    refresh();
 
+    $scope.addContact = function(){
+        console.log('adding new contact to db');
+        console.log($scope.contact);
+        $http.post('/contactlist', $scope.contact).success(function (response) {
+            console.log(response);
+            refresh();
+        });
+    };
+
+    $scope.deleteContact = function (id) {
+        console.log('deleting contact: ' + id);
+        $http.delete('/contactlist/' + id).success(function (response) {
+            console.log(response);
+            refresh();
+        });
+
+    };
 
 }]);
--- server.js
+++ server.js
@@ -2,22 +2,42 @@ var express = require('express');
 var app = express();
 var mongojs = require('mongojs');
 var db = mongojs('contactlist', ['contactlist']);
+var bodyParser = require('body-parser');
 
-var PORT_NUM = 3000
+var PORT_NUM = 3000;
 
 /*app.get('/', function (request, response){
-	response.send("Hello world from server.js");
-});*/
+ response.send("Hello world from server.js");
+ });*/
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
 
-app.get('/contactlist', function (request, response){
-	console.log("I received a GET request");
+app.get('/contactlist', function (request, response) {
+    console.log("I received a GET request");
 
-	db.contactlist.find(function (err, docs) {
-		console.log(docs);
-		response.json(docs);
-	});
-})
+    db.contactlist.find(function (err, docs) {
+        console.log(docs);
+        response.json(docs);
+    });
+});
+
+app.post('/contactlist', function (request, response) {
+    console.log('I receive a post request');
+
+    console.log(request.body);
+    db.contactlist.insert(request.body, function (err, docs) {
+        response.json(docs);
+    });
+});
+
+app.delete('/contactlist/:id', function (request, response) {
+    var id = request.params.id;
+    console.log(id);
+    db.contactlist.remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
+             response.json(doc);
+    });
+});
+
 
 app.listen(PORT_NUM);
 console.log("server running on port 3000");
